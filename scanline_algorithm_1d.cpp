84a4fcacb2e7dd1ddd71be0b441fe32e544ee5e8
Rohitkumarvarma-369 <79484529+Rohitkumarvarma-369@users.noreply.github.com>
2021-05-12 23:29:46 +0530

    Add files via upload

---
+++ scanline_algorithm_1d.cpp
@@ -0,0 +1,48 @@
+/*
+Rohit Kumar Varma (@immortal_1)
+Amrita University, Amritapuri, 2019-2023
+Scanline algorithm
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define for0(i, n) for (int i = 0; i < n; i++)
+#define for1(i, n) for (int i = 1; i <= n; i++)
+#define loop(i,a,b) for (int i = a; i < b; i++)
+#define bloop(i,a,b) for (int i = a ; i>=b;i--)
+int main(){
+	int n;
+	cin>>n;
+	int array[n];
+	for0(i,n){
+		cin>>array[i];
+	}
+
+	//pre-processing
+	int scanline[n+1] = {0};
+	int q;
+	cin>>q;
+	while(q--){
+		int l,r,x;
+		cin>>l>>r>>x;
+		scanline[l] = scanline[l]+x;
+		scanline[r+1] = scanline[r+1]-x;
+	}
+	for0(i,n+1){
+		cout<<scanline[i]<<" ";
+	}
+	cout<<endl;
+
+	
+	//post-processing
+	int scan = 0;
+	for0(i,n){
+		scan = scan+scanline[i];
+		array[i] = array[i]+scan;
+	}
+	for0(i,n){
+		cout<<array[i]<<" ";
+	}
+
+}
